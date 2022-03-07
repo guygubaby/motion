f86805bdc34ba4fc73a74ee0b050d477c7a2f50f
guygubaby <guygubaby@gmail.com>
2022-03-07 12:45:41 +0800

    fix: fix gsap Flip playground demo

--- playground/src/pages/flip/index.vue
+++ playground/src/pages/flip/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="h-50vh flex justify-center items-center">
     <div class="flex flex-col">
-      <img ref="sRef" class="thumbnail w-200px h-200px" data-flip-id="img" src="https://placehold.co/200x200" @click="toogle">
+      <img ref="sRef" class="thumbnail" data-flip-id="img" src="https://placehold.co/100x100" @click="toogle">
 
-      <div class="container" @click="toogle">
-        <img ref="eRef" class="full-size" data-flip-id="img" src="https://placehold.co/600x600">
+      <div v-if="isShow" class="inset-0 z-999 bg-gray-100 fixed flex justify-center items-center" @click="toogle">
+        <img ref="eRef" data-flip-id="img" src="https://placehold.co/300x300">
       </div>
     </div>
   </div>
@@ -14,72 +14,70 @@
 import { gsap } from 'gsap'
 import { Flip } from 'gsap/Flip'
 import { Draggable } from 'gsap/Draggable'
-import { onMounted, ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 
 gsap.registerPlugin(Flip, Draggable)
 
 const sRef = ref()
 const eRef = ref()
 
+const isShow = ref(false)
+
 onMounted(() => {
   Draggable.create(sRef.value)
 })
 
-const toogle = () => {
-  const state = Flip.getState([sRef.value, eRef.value])
+const loadImg = (url: string) => {
+  return new Promise<void>((resolve, reject) => {
+    const img = new Image()
+    img.onload = () => resolve()
+    img.onerror = () => reject(new Error('Failed to load image'))
+    img.src = url
+  })
+}
 
-  eRef.value.classList.toggle('active')
+const show = async() => {
+  isShow.value = true
+  await nextTick()
+  await loadImg(eRef.value.src)
+
+  const state = Flip.getState([sRef.value, eRef.value])
+  Flip.from(state, {
+    fade: true,
+    duration: 0.9,
+    absolute: true,
+    ease: 'power1.inOut',
+    toggleClass: 'flipping',
+  })
+}
+
+const hide = () => {
+  const state = Flip.getState([sRef.value, eRef.value])
+  Flip.to(state, {
+    fade: true,
+    duration: 0.9,
+    absolute: true,
+    ease: 'power1.inOut',
+    toggleClass: 'flipping',
+    onComplete: () => {
+      isShow.value = false
+    },
+  })
+}
+
+const toogle = async() => {
   sRef.value.classList.toggle('active')
 
-  Flip.from(state, {
-    duration: 0.6,
-    fade: true,
-    absolute: true,
-    toggleClass: 'flipping',
-    ease: 'power1.inOut',
-  })
+  if (isShow.value)
+    hide()
+
+  else
+    await show()
 }
 
 </script>
 
 <style lang="css" scoped>
-body {
-  color: white;
-  background-color: black;
-  padding: 0;
-  font-family: "Signika Negative", Arial, sans-serif;
-  box-sizing: border-box;
-  text-align: center;
-}
-
-h1 {
-margin-left: 20px;
-}
-
-.container {
-  border: 1px solid rgba(255,255,255,0.3);
-  position: fixed;
-  top: 15px;
-  left: 15px;
-  right: 15px;
-  bottom: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  pointer-events: none;
-}
-
-.full-size {
-  flex-shrink: 0;
-  flex-grow: 0;
-  height: 100%;
-  display: none;
-}
-
-.full-size.active {
-  display: block;
-}
 
 .thumbnail.active {
   visibility: hidden;
